68868bceba397ec0409bca1719497438f6d61bfd
Sanaulla Shaikh <sanaulla@0101digitall.com>
2025-07-25 01:17:50 +0530

    fix: Update icon imports to match new file names and paths

--- src/pages/home/components/find-center/index.tsx
+++ src/pages/home/components/find-center/index.tsx
@@ -12,9 +12,9 @@ import SearchIcon from "@mui/icons-material/Search";
 import { useNavigate } from "react-router-dom";
 import searchIcon from "@/assets/icons/search.png";
 import doctor from "@/assets/icons/doctors.png";
-import drugstore from "@/assets/icons/drugstore.png";
+import drugstore from "@/assets/icons/drugstores.png";
 import hospital from "@/assets/icons/hospitals.png";
-import capsule from "@/assets/icons/capsule.png";
+import capsule from "@/assets/icons/capsules.png";
 import ambulance from "@/assets/icons/ambulance.png";
 
 import { UIButton } from "@/ui-kit/button";
